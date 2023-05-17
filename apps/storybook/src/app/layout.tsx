57168b715c0db2eb5f88309c38b4b4f4858a8a4f
Terris Kremer <terris.kremer@gmail.com>
2023-05-17 03:47:37 -0400

    add storybook

---
+++ apps/storybook/src/app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Curio : Storybook",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
